4956be16cd94d73ca38645a6e501493fa7c8afb3
nixaty <nixaty@nixaty.ru>
2025-08-20 09:02:46 +0400

    Add static files import

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi import Request
 
 
 app = FastAPI()
